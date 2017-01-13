8218e4e6050545f3bd364665a7d960ed2dd8358f
William Palfreman <william@palfreman.com>
2017-01-13 17:39:50 +0000

    Everything should work

--- lizard-relay/lizard-relay.ino
+++ lizard-relay/lizard-relay.ino
@@ -4,9 +4,9 @@
 
 const int cold_r = 9; // Pin of relay for cold light, e.g. UVB
 const int hot_r = 10; // Pin of relay for hot relay, e.g. heater
-const unsigned long read_delay = 60; // Seconds delay between temp readings.
+const unsigned long read_delay = 60; // Seconds delay between temp readings
 const int too_hot = 30; // Temp *C that is too hot
-const int too_cold = 22; // Temp *C that is too cold. 18 in use and room temp to test in your hand.
+const int too_cold = 18; // Temp *C that is too cold
 const int fence = 2; // Amount *C temp must move before changing. To stop flicker.
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -14,16 +14,25 @@ void setup() {
     delay(5000); // Open serial monitor
     dht.begin();
     Serial.begin(9600);
+    Serial.print("Thresholds: too_cold=");
+    Serial.print(too_cold);
+    Serial.print("C : too_hot=");
+    Serial.print(too_hot);
+    Serial.print("C : fence=");
+    Serial.print(fence);
+    Serial.print("C : read_delay=");
+    Serial.print(read_delay);
+    Serial.println("s");
     pinMode(cold_r,OUTPUT);
     pinMode(hot_r,OUTPUT);
     pinMode(DHTPIN,INPUT_PULLUP);
     Serial.println("Test, Going LOW");
-    digitalWrite(cold_r,LOW);
-    digitalWrite(hot_r,LOW);
+    digitalWrite(cold_r,LOW);           // Strip light off
+    digitalWrite(hot_r,LOW);            // Heater on
     delay(5000);
-    Serial.println("Test, Going HIGH");
-    digitalWrite(cold_r,HIGH);
-    digitalWrite(hot_r,HIGH);
+    Serial.println("Test, Going HIGH"); // The default state when unpowered or broken
+    digitalWrite(cold_r,HIGH);          // Strip light on
+    digitalWrite(hot_r,HIGH);           // Heater off
     delay(5000);
     }
 
@@ -45,14 +54,17 @@ void loop() {
             Serial.print(t);
             Serial.print("C. Waiting for temp to fall below ");
             Serial.print(too_hot - fence);
-            Serial.println("C");
+            Serial.print("C. ");
             delay(read_delay*1000);
             t = get_temp_wait();
+            // get_temp_wait can turn the strip light on if it gets over ten bad readings. Make sure it is off.
+            Serial.println("Set cold_r LOW (strip light off)");
+            digitalWrite(cold_r,LOW);
         }
         Serial.print("Temp no longer too hot @ ");
         Serial.print(t);
-        Serial.println("C. Turning light back on");
-        digitalWrite(cold_r,HIGH); // Strip light on
+        Serial.println("C. Turning strip light back on");
+        digitalWrite(cold_r,HIGH);
     }
 
     // Temperature is under minium
@@ -60,7 +72,7 @@ void loop() {
         digitalWrite(cold_r,HIGH); // Strip light on
         digitalWrite(hot_r,LOW); // Heater on
         Serial.print(t);
-        Serial.println("C. Too cold. Set cold relay HIGH (strip light on) and hot relay LOW (heater on)");
+        Serial.println("C is too cold. Set cold relay HIGH (strip light on) and hot relay LOW (heater on)");
 
         // Wait until it warms up
         while ((t - fence) < too_cold) {
@@ -68,9 +80,12 @@ void loop() {
             Serial.print(t);
             Serial.print("C. Waiting for temp to rise above ");
             Serial.print(too_cold + fence);
-            Serial.println("C");
+            Serial.print("C. ");
             delay(read_delay*1000);
             t = get_temp_wait();
+            //  get_temp_wait can turn the heater off after ten bad readings. Make sure it is on.
+            Serial.println("Set hot_r LOW (heater on)");
+            digitalWrite(hot_r,LOW);
         }
         Serial.print("Temp normal @ ");
         Serial.print(t);
